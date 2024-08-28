7355aae6aaf2492506b05cb69c16d0ca0d33d333
castlectry <122072831+newandthe@users.noreply.github.com>
2024-08-28 14:19:42 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,30 +1,50 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		int five = N/5;
-		int left_kg = N%5;
-		
-		int ans = -1;
-		
-		
-		
-		for (int i = five; i >= 0; i--) {	// 최대한 적은 봉지 개수를 들기 위해 5키로로 최대한 나눈다.
+    static int N;
 
-			if(left_kg % 3 == 0) {
-				ans = i + (left_kg/3);
-				
-				break;
-			}
-			
-			left_kg += 5;
-		}
-		
-		System.out.println(ans);
-	}
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        boolean isSuccess = false;
+
+        /*
+        * N을 5와 3으로 나눈 몫의 합 최소 값 구하는 문제
+        * 구할 수 없는 경우 -1 출력
+        * */
+
+        int tmp_5 = N / 5;    // 일단 5로 나눠서 봉지 수 최대의 경우 수 취득
+        int tmp_3 = 0;
+        int tmpMod = N % 5;   // 나머지 값 취득
+//        System.out.println("############");
+//        System.out.println(tmp_5);
+//        System.out.println(tmpMod);
+//        System.out.println("############");
+
+        endPoint:
+        while (tmp_5 >= 0) {
+
+            if (tmpMod % 3 == 0) {
+//                System.out.println("A");
+//                System.out.println("5: " + tmp_5 + " 3: " + tmp_3 + " mod: " + tmpMod);
+                System.out.println(tmp_5 + (tmpMod / 3)  );
+                isSuccess = true;
+                break endPoint;
+            }
+
+            tmp_5--; tmpMod += 5;
+
+        }
+
+        if (!isSuccess) {
+            System.out.println(-1);
+        }
+
+    }
 }
