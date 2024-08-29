26308c8c34a13cb8781bf5d742e291cd65f21165
castlectry <122072831+newandthe@users.noreply.github.com>
2024-08-29 12:26:36 +0900

    [Silver III] Title: 2×n 타일링, Time: 100 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,37 +1,40 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		
-		int dp[] = new int[N];
-		
-		if(N == 1) {
-			System.out.println(1);
-		}
-		
-		else if (N == 2) {
-			System.out.println(2);
-		}
-		
-		else {
-			dp[0] = 1;
-			dp[1] = 2;
-			
-			for (int i = 2; i < N ; i++) {
-				dp[i] = (dp[i-1] + dp[i-2])%10007;
-			}
-			
-			System.out.println(dp[N-1]%10007);
-		}
-		
-		
-		// System.out.println(Arrays.toString(dp));
-	}
+    static int[] dp;
+    static int result;
 
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        dp = new int[n+1];
+
+        if(n == 1) {
+            System.out.println(1);
+        } else if (n == 2) {
+            System.out.println(2);
+        } else {
+
+            dp[1] = 1;
+            dp[2] = 2;
+
+            for (int i = 3; i <= n; i++) {
+
+                // 맨 오른쪽이 2 * 1 인 경우의 수 + 1 * 2 인 경우의 수
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+
+            }
+
+            System.out.println(dp[n]);
+        }
+
+
+
+    }
 }
